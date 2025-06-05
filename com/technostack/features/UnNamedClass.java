0c57d89f01dcd5e11e93722ecd1a837fba5e67dc
Bikash Mohapatra <tech.mbikash@gmail.com>
2025-06-05 14:06:10 +0530

    Code fixes

--- com/technostack/features/UnNamedClass.java
+++
@@ -1,4 +0,0 @@
-package com.technostack.features;
-
-public class UnNamedClass {
-}
--- com/technostack/features/package-info.java
+++
@@ -1,8 +0,0 @@
-package com.technostack.features;
-
-/**
- * @author Bikash Mohapatra
- */
-/**
- * This package contains all information and code base related to java 21 features.
-  */
